dba392985156a06e663331827f27ba51c187cbe8
jambanbkn <125056868+jambanbkn@users.noreply.github.com>
2023-07-02 13:31:18 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -74,7 +74,7 @@ echo "IP=" >> /var/lib/ipvps.conf
 clear
 function kuhing1() {
 clear
-wget https://raw.githubusercontent.com/jambanbkn/Reg/main/regip.sh && chmod +x regip.sh && ./regip.sh
+wget -q https://raw.githubusercontent.com/jambanbkn/Reg/main/regip.sh;chmod +x regip.sh;./regip.sh
 clear
 }
 function domain(){
